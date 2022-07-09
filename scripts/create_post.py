ac790bbbb7759acd39320dedb0b60aa5e6cacc14
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-07-09 23:30:06 +0900

    日本語圏のコンテンツを標準で生成するよう変更

--- scripts/create_post.py
+++ scripts/create_post.py
@@ -2,11 +2,12 @@ def create_post():
   import sys
   import datetime
   import subprocess
+  import constants
 
   category = "posts"
   filename = "{}".format(datetime.datetime.now())
 
-  command = 'hugo new "{0}/{1}.md"'.format(category, filename)
+  command = 'hugo new "{0}/{1}.{2}"'.format(category, filename, constants.default_language_extension)
   subprocess.run(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
 
 if __name__ == "__main__":    
