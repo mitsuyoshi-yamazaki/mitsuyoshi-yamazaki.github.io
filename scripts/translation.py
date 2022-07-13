d53dead57014fdadca70fefd7e17238a9adb239e
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-07-13 23:47:03 +0900

    自動生成記事に説明メモを追加

--- scripts/translation.py
+++ scripts/translation.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 
+from ast import parse
 import re
-from typing import Callable
+from typing import Collection, Tuple, Optional
 from utilities import Language, get_translated_filepath, log
 
 markdown_ignore_lines = [
@@ -37,56 +38,96 @@ class MarkdownLine:
     self.content = raw_line.replace(self.prefix, "", 1)
     return
 
-Translator = Callable[[str], str]
+class MarkdownParser:
+  hugo_control_header: Optional[str]
+  lines: Collection[MarkdownLine]
+
+  def __init__(self, contents: str):
+    def split_contents() -> Tuple[Optional[str], str]:
+      hugo_control_header_match = re.search(hugo_control_header_pattern, contents)
+      if hugo_control_header_match is None:
+        log("Hugo control header not found")
+        return (None, contents)
+      
+      header = hugo_control_header_match.group()
+      log("Hugo control header found:\n{}".format(header))
+      return (
+        header,
+        contents.replace(header, "", 1)
+      )
+
+    parsed = split_contents()
+    self.hugo_control_header = parsed[0]
+    self.lines = [MarkdownLine(line) for line in parsed[1].split("\n")]
+
+class Translator:
+  language: Language
+  translating_comment: Optional[str]
+
+  def __init__(self):
+    pass
+
+  def translate(self, line: str) -> str:
+    raise NotImplementedError("Use concrete Translator classes")
+
+class EnglishTranslator(Translator):
+  def __init__(self):
+    self.language = Language.ENGLISH
+    self.translating_comment = "*This article is automatically generated from the original content*"
+
+  def translate(self, line: str) -> str:
+    return line # TODO:
+    # return " [{0}] {1}".format(language.value, line) if len(line) > 0 else ""  # TODO:
 
 def get_translator(language: Language) -> Translator:
   if language == Language.ENGLISH:
-    return lambda x: " [{0}] {1}".format(language.value, x) if len(x) > 0 else ""  # TODO:
+    return EnglishTranslator()
   elif language == Language.JAPANESE:
     raise ValueError("Unsupported target language {}".format(language))
   else:
     raise ValueError("Undefined language code {}".format(language))
 
-def translate(contents: str, translator: Translator) -> str:
-  hugo_control_header_match = re.search(hugo_control_header_pattern, contents)
-  hugo_control_header = ""
-  if hugo_control_header_match is not None:
-    hugo_control_header = hugo_control_header_match.group()
-    contents = contents.replace(hugo_control_header, "", 1)
-    log("Hugo control header found:\n{}".format(hugo_control_header))
-  else:
-    log("Hugo control header not found")
+def translate(contents: str, language: Language) -> str:
+  markdown_parser = MarkdownParser(contents)
+  translator = get_translator(language)
 
   def translate_line(line: MarkdownLine) -> str:
-    result = line.prefix + translator(line.content)
+    result = line.prefix + translator.translate(line.content)
     if line.raw_content:
       log("{0}=={1}\n{2}\n".format(line.prefix, line.content, result))
     return result
 
-  lines = [MarkdownLine(x) for x in contents.split("\n")]
-  translated_lines = [translate_line(line) for line in lines]
-  return hugo_control_header + "\n".join(translated_lines)
+  translated_contents = [
+  ]
+  if markdown_parser.hugo_control_header is not None:
+    translated_contents.append(markdown_parser.hugo_control_header)
+  if translator.translating_comment is not None:
+    translated_contents.append(translator.translating_comment)
+  translated_contents.extend([translate_line(line) for line in markdown_parser.lines])
+  
+  return "\n".join(translated_contents)
 
 if __name__ == "__main__":
   import os
   import argparse    
 
   parser = argparse.ArgumentParser(description="Translate a markdown file")
-  parser.add_argument("filepath", metavar="filepath", type=str, help="filepath to translate")
-  parser.add_argument("language", metavar="language", type=str, help="language that it translates to")
+  parser.add_argument("filepath", type=str, help="filepath to translate")
+  parser.add_argument("language", type=str, help="language that it translates to")
+  parser.add_argument("-f", action="store_true", help="force replacing the old translation file")
 
   parsed = parser.parse_args()
   filepath = parsed.filepath
   language = Language(parsed.language)
-  translator = get_translator(language)
   translated_filepath = get_translated_filepath(filepath, language)
 
   if os.path.exists(translated_filepath):
-    raise ValueError("File already exists at path {}".format(translated_filepath))
+    if parsed.f != True:
+      raise ValueError("File already exists at path {}".format(translated_filepath))
 
   with open(filepath, "r") as f:
     contents = f.read()
-    translated = translate(contents, translator)
+    translated = translate(contents, language)
     with open(translated_filepath, "w") as o:
       o.write(translated)
       print("Translation completed: {}".format(translated_filepath))
