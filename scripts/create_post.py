7dc6697fab4bc4d4096c23798cd29bbc05fc3275
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-08-07 14:08:36 +0900

    ファイル名にタイトルを表記

--- scripts/create_post.py
+++ scripts/create_post.py
@@ -1,16 +1,25 @@
 # coding:utf-8
 
-def create_post():
+def create_post(filename: str):
   import sys
   import datetime
   import subprocess
   import utilities
 
   category = "posts"
-  filename = "{}".format(datetime.datetime.now())
 
   command = 'hugo new "{0}/{1}.{2}"'.format(category, filename, utilities.default_language_extension)
   subprocess.run(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
 
 if __name__ == "__main__":    
-  create_post()
+  import argparse    
+  import datetime
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("title", type=str)
+
+  title = parser.parse_args().title
+  # filename = "{}".format(datetime.datetime.now())
+  filename = "{0}_{1}".format(datetime.datetime.now().date(), title)
+
+  create_post(filename)
